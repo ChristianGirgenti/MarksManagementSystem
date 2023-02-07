33523274a6960795fd5ec05d091b01c10e3bc6f9
christian595 <christiangirgenti595@gmail.com>
2023-02-07 15:12:37 +0000

    CG-1: Create first page, playing around with Razor and create a working nav bar

---
+++ MarksManagementSystem/Pages/Course/ViewAllCourses.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MarksManagementSystem.Pages.Course
+{
+    public class ViewAllCoursesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
