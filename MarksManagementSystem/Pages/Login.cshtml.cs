1f92664e2b67b59cf24f3443520d59625b265e3b
christian595 <christiangirgenti595@gmail.com>
2023-02-20 00:24:24 +0000

    draft of login backend

--- MarksManagementSystem/Pages/Login.cshtml.cs
+++ MarksManagementSystem/Pages/Login.cshtml.cs
@@ -1,31 +1,42 @@
+using MarksManagementSystem.Data.Models;
+using MarksManagementSystem.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace MarksManagementSystem.Pages
 {
     public class LoginModel : PageModel
     {
+        private MarksManagementContext _marksManagementContext;
+
+        public LoginModel(MarksManagementContext context)
+        {
+            _marksManagementContext = context;
+        }
+
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public string ErrorMessage { get; set; }
         public void OnGet()
         {
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            string email = Request.Form["email"];
-            string password = Request.Form["password"];
+            if (!ModelState.IsValid) return Page();
+            var teacher = await _marksManagementContext.Teachers.FirstOrDefaultAsync(x => x.Email == Credential.Email);
 
-            Console.WriteLine(email+ " " + password);
-
-            bool isLoginSuccessfull = true;
-            bool isTeacher = true;
-
-            //If is teacher and admin redirect to admin view
-            //Else is teacher and not admin redirect to teacher view
-            //Else rredirect to student view
-
-            if (isLoginSuccessfull && isTeacher)
+            if (teacher != null && teacher.Password == Credential.Password)
+            {
                 return RedirectToPage("/Teachers/AddTeacher");
-            else return RedirectToPage("Students/AddStudent");                
+            }
+            else
+            {
+                ErrorMessage = "Invalid ursername or password";
+                return Page();
+            }
         }
     }
 }
