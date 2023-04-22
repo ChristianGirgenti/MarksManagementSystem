5f9e26e109bc9e3a950048f83b7871ee7c3907ef
christian595 <christiangirgenti595@gmail.com>
2023-04-22 17:45:18 +0100

    Minor changes

--- MarksManagementSystem/Pages/Account/ChangePassword.cshtml.cs
+++ MarksManagementSystem/Pages/Account/ChangePassword.cshtml.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authorization;
-using MarksManagementSystem.Data.Models;
 using MarksManagementSystem.Services.Interfaces;
 
 namespace MarksManagementSystem.Pages.Account
